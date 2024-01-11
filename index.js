5ea64f356923ed241a8ad00b136533ff92b117d9
mirzaakhmedov20 <mirzaakhmedovshukurullo20gmail.com>
2024-01-11 21:53:28 +0500

    js 2-month

---
+++ index.js
@@ -0,0 +1,70 @@
+// 1. Berilgan massiv elementlari orasida qatnashgan sonlar yig'indisini topuvchi  funksiya yozing.
+//  [1, 2, "salom", true, "xayr"]
+// function sumSonlar(arr) {
+//   let sum = 0;
+//   for (i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] == "number") {
+//       i+2;
+//     }
+//   }
+//   return sum;
+// }
+// const arr = [1, 2, "salom", true, "xayr", 5];
+// console.log(sumSonlar(arr));
+
+// 2. Berilgan yozuvning so'zlarini bosh harflariga almashtiradigan funksiya yozing
+//  lorem ipsum => "Lorem Ipsum"
+
+// let res = str.replace(function capitalizSoz(txt) {
+//   return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+// });
+// const str = "javascript capitalize string";
+// console.log(capitalizSoz(str));
+// function capitalizeWord(txt) {
+//     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//   }
+//   let str = "javascript capitalize string";
+//   let res = str.replace(/(\b[a-z])/g, capitalizeWord);
+
+//   console.log(res);
+
+// 3. Ixtiyoriy berilgan yozuvni palindromlikka tekshiradigan funksiya yozing.
+// palindrom -> to'g'ri o'qilsa ham teskari o'qilsa ham bir xil o'qiladigan son
+// function isPalindrome(str) {
+//   const reversedStr = str.split("").reverse().join("");
+//   return str == reversedStr;
+// }
+
+// const str = "madam";
+// console.log(isPalindrome(str)); // true
+// function isPalindrome(str) {
+//   const reversedStr = str.split("").reverse().join("");
+//   return str == reversedStr;
+// }
+// const str = "";
+// console.log(isPalindrome(str));
+
+// 4. Obyektlardan iborat massiv berilganda ular orasidan eng katta yoshlisining nomini
+//  chiqaradigan funksiya yozing.
+// const users = [
+// {
+// id: 1,
+// name: "John",
+// age: 40
+// },
+// }
+// id: 2,
+// name: "Bill",
+// age: 45
+// },
+// {
+// id: 3,
+// name: "Adam",
+// age: 30
+// }
+// // ];
+try {
+    
+} catch (err) {
+    
+}
